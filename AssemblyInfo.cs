0c46ad835485f1e8be42144570eb7d0c087441af
rakium <karenoferlc4+temp@gmail.com>
2024-11-17 15:00:40 +0000

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("XWorm by @Code2Reverse")]
+[assembly: AssemblyDescription("XCoder")]
+[assembly: AssemblyCompany("@Code2Reverse")]
+[assembly: AssemblyProduct("XWorm")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("@Code2Reverse")]
+[assembly: ComVisible(false)]
+[assembly: Guid("da0e7b82-9a56-4026-af48-52629b0d254b")]
+[assembly: AssemblyFileVersion("5.6.0.0")]
+[assembly: AssemblyVersion("5.6.0.0")]
